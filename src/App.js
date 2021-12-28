054c94bf6e9ddc5bab3ded3d4a1a6466c33ca028
Mario <mamerida2013@gmail.com>
2021-12-28 10:26:45 -0300

    creacion del proyecto con styled component y primer ejemplo de uso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+//creo el elemento utilizando los componentes de styles y agrego las propieades css
+const P = styled.p`
+  font-size:24px;
+  color:red;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <P>Hola soy un parrafo</P>
     </div>
   );
 }
