f2807d4db56ed3151eaa85a8bcb7a8b2965b2064
Mario <mamerida2013@gmail.com>
2021-12-28 10:37:45 -0300

    utilizando renderizado condicional con styled-component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,29 @@ const P = styled.p`
 
 `
 
+const Content = styled.div`
+  padding:20px 25px;
+
+`
+
+//se crea renderizado condicional mediante los templade strings 
+//en este caso en caos de existir la propiedad de primario pondra un boton u otro
+const Button = styled.button`
+  background-color : ${props => props.primary ? 'red' : 'white '};
+  color: ${props => props.primary ? 'white' : 'red '};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius:4px;
+`
+
 function App() {
   return (
-    <div>
+    <Content>
       <P>Hola soy un parrafo</P>
-    </div>
+      <Button>Enviar </Button>
+      <Button primary={"hola"}>Enviar </Button>
+    </Content>
+    
   );
 }
 
